8d82b14ae46bb060ee80e331664811f2c3f7830b
Nicolas Gonzalez <ndgfrs@gmail.com>
2022-09-10 03:40:25 -0400

    Add basic translation check.

--- index.js
+++ index.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const app = express();
-const phrases = require('./static/json/phrases.json').greetings;
 
 app.use('/static', express.static('static'));
 app.use(express.urlencoded({ extended: true }));
@@ -10,11 +9,48 @@ app.listen(8000, () => {
     console.log(`Listening on: http://localhost:8000`);
 });
 
-app.get('/', (request, response) => {
-    let prompt = phrases[Math.floor(Math.random() * phrases.length)];
-    let params = {
-        phrase: prompt.phrase,
-        valid_answers: prompt.valid_answers,
+// ---------------------------------------------------------------------------
+
+// Prepare the string for case-insensitive comparison.
+function cleanupString(string) {
+    return string.replace(/[.,\/#!$%\^&\*;:{}=\-_`~()]/g, "").toLowerCase();
+}
+
+// Helper function to clean all valid answers of a phrase.
+function cleanupValidAnswers(phrasesCategory) {
+    for (let index = phrasesCategory.length - 1; index--; ) {
+        let valid_answers = phrasesCategory[index].valid_answers;
+        valid_answers = valid_answers.map((answer) => cleanupString(answer));
+        phrasesCategory[index].valid_answers = [...new Set(valid_answers)];
+    }
+    return;
+}
+
+function getParameters(index) {
+    const phrasesGreetings = require('./static/json/phrases.json').greetings;
+    cleanupValidAnswers(phrasesGreetings);
+    if ((index < 0) || (index >= phrasesGreetings.length)) {
+        return null;
+    }
+    return {
+        phrase: phrasesGreetings[index].phrase,
+        valid_answers: phrasesGreetings[index].valid_answers,
     };
-    response.render('index.ejs', params);
+}
+
+// For incrementing the index of the phrase.
+let index = 0;
+
+// ---------------------------------------------------------------------------
+
+app.get('/', (request, response) => {
+    response.render('index.ejs', getParameters(index));
+});
+
+app.post('/translate', (request, response) => {
+    let translation = request.body.translation;
+    if (getParameters(index).valid_answers.includes(cleanupString(translation))) {
+        index++;
+    }
+    response.redirect('/');
 });
